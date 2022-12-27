5070974f33e619a58c4674f86b22ddcff0c33f38
Ahmed Sayed Ali <mrahmedsayedali@gmail.com>
2022-12-27 19:27:38 +0200

    List 🌝

--- main.dart
+++ main.dart
@@ -7,5 +7,20 @@ github : https://github.com/MrAhmedSayedAli
 
 main() {
   print('Ahmed Elsayed');
+
+
+  List myList = ['Ahmed','Mohamed'];
+  print(myList);
+  myList.add('Amit');
+  print(myList);
+  myList.addAll(['Ali','Zeinab']);
+  print(myList);
+
+  myList.insert(2, 'NameElse');
+  print(myList);
+
+  myList.insertAll(3, ['NameElse1','NameElse2']);
+  print(myList);
+
 }
 
